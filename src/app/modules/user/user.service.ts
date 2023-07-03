19e92fd36a4b75df1aca47053ba7d40b82251cb6
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-07-03 09:44:25 +0600

    academicSemester schema & model

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,25 +1,25 @@
-import config from '../../../config'
-import { IUser } from './user.interface'
-import { User } from './user.model'
-import { generateUserId } from './user.utils'
+import config from '../../../config';
+import { IUser } from './user.interface';
+import { User } from './user.model';
+import { generateUserId } from './user.utils';
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
   // ---> auto generated password
-  const id = await generateUserId()
-  user.id = id
+  const id = await generateUserId();
+  user.id = id;
   // ---> default password
   if (!user.password) {
-    user.password = config.default_user_pass as string
+    user.password = config.default_user_pass as string;
   }
 
   //   ---> created id
-  const createNewUser = await User.create(user)
+  const createNewUser = await User.create(user);
   if (!createNewUser) {
-    throw new Error('Failed to create user!')
+    throw new Error('Failed to create user!');
   }
-  return createNewUser
-}
+  return createNewUser;
+};
 
 export const UserService = {
   createUser,
-}
+};
--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,46 @@
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config'
-import { logger, errorLogger } from './shared/logger'
-import { Server } from 'http'
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+import { logger, errorLogger } from './shared/logger';
+import { Server } from 'http';
 
 // ---> uncaught exception error handle
 process.on('uncaughtException', error => {
-  errorLogger.error(error)
-  process.exit(1)
-})
+  errorLogger.error(error);
+  process.exit(1);
+});
 
-let server: Server
+let server: Server;
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string)
-    logger.info('database connected successfully')
+    await mongoose.connect(config.database_url as string);
+    logger.info('database connected successfully');
 
     server = app.listen(config.port, () => {
-      logger.info(`Application listening on port ${config.port}`)
-    })
+      logger.info(`Application listening on port ${config.port}`);
+    });
   } catch (err) {
-    errorLogger.error('failed to connect database', err)
+    errorLogger.error('failed to connect database', err);
   }
 
   process.on('unhandledRejection', error => {
     if (server) {
       server.close(() => {
-        errorLogger.error(error)
-        process.exit(1)
-      })
+        errorLogger.error(error);
+        process.exit(1);
+      });
     } else {
-      process.exit(1)
+      process.exit(1);
     }
-  })
+  });
 }
 
-main()
+main();
 
 // ---> sigterm
 process.on('SIGTERM', () => {
-  logger.info('SIGTERM is received')
+  logger.info('SIGTERM is received');
   if (server) {
-    server.close()
+    server.close();
   }
-})
+});
